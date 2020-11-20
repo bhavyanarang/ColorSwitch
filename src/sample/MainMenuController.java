816c956dd1612177fc81f4c1031d576ce5a3cc21
bhavyanarang <bhavyanarang09@gmail.com>
2020-11-21 03:32:45 +0530

    Removing some errors

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -1,18 +1,7 @@
 package sample;
-import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.scene.transform.Rotate;
-import javafx.stage.Stage;
 public class MainMenuController {
 
     @FXML private Group circle1;
@@ -32,9 +21,8 @@ public class MainMenuController {
     }
 
 
-    @Override
-    public void handle(){
-
+    public MainMenuController(){
+        rotate();
     }
 
 }
