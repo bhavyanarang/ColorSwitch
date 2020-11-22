77d761ef9e8084b38f65f306997ce35be2787211
dipankarjiwani <dipankar19037@iiitd.ac.in>
2020-11-22 21:44:06 +0530

    added

--- src/sample/Star.java
+++ src/sample/Star.java
@@ -1,5 +1,16 @@
 package sample;
 
-public class Star {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+public class Star {
+    Image img;
+    Star(){
+        img = new Image("Assets/star1.png");
+        ImageView view = new ImageView(img);
+    }
+
+    public Image getImg() {
+        return img;
+    }
 }
