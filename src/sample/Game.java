05e2e0da283fa91d8c553681afca21873616de6c
dipankarjiwani <dipankar19037@iiitd.ac.in>
2020-12-04 19:47:17 +0530

    fundamnetal_hitcheck,random_obstacles

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -2,11 +2,13 @@ package sample;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
-    AnchorPane pane;
+    Pane pane;
     private Scorecard scorecard;
     private Star star;
     private Ball ball;
@@ -14,14 +16,26 @@ public class Game {
     private ColorSwitcher colorSwitcher;
     private Pause pause;
     private Player player;
+    private ArrayList<Obstacle> Obstacles;
     public void initialize(){
 
     }
-    public void startNewGame(){
-        this.pane.getChildren().setAll(ball.getBall(), pause.getPauseButton(),scorecard.getLabel(),star.getImg());
-    }
-    private boolean didHit(){
+    public void startNewGame() {
+        int Obstaclenumber=new Random().nextInt(8-1)+1;
+        Obstacle presentOb=Obstacles.get(Obstaclenumber-1);
+        //if(Obstaclenumber==2 || Obstaclenumber==3 || Obstaclenumber==5 || Obstaclenumber==6 || Obstaclenumber==7 || Obstaclenumber==8){
+            this.pane.getChildren().setAll(ball.getBall(), pause.getPauseButton(), scorecard.getLabel(), star.getImg(),presentOb.returnObstacle(),presentOb.returnObstacle2());
+        //}
 
+    }
+    private boolean didHit(Obstacle presentOb){
+        for (Shape shape:presentOb.components){
+            Shape intersect = Shape.intersect(shape, ball.getBall());
+            if (intersect.getBoundsInLocal().getWidth() != -1) {
+                System.out.println("OOps");
+                return true;
+            }
+        }
         return false;
     }
     private boolean didPause(){
@@ -30,7 +44,7 @@ public class Game {
     private boolean didHitStar(){
         return false;
     }
-    public Game(AnchorPane p){
+    public Game(Pane p){
         this.pane=p;
         scorecard=new Scorecard();
         star=new Star();
@@ -39,6 +53,15 @@ public class Game {
         colorSwitcher=new ColorSwitcher();
         pause=new Pause();
         player=new Player();
+        Obstacles=new ArrayList<Obstacle>(0);
+        Obstacles.add(new Obstacle1(200,200));
+        Obstacles.add(new Obstacle2(200,200));
+        Obstacles.add(new Obstacle3(200,200));
+        Obstacles.add(new Obstacle4(200,200));
+        Obstacles.add(new Obstacle5(200,200));
+        Obstacles.add(new Obstacle6(200,200));
+        Obstacles.add(new Obstacle7(200,200));
+        Obstacles.add(new Obstacle8(200,200));
     }
 
 
